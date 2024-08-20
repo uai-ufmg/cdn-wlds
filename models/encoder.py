07a200fa93c881664e38111ac0507f49bf0b0928
Henrique Magalhães de Oliveira Carvalho - PrestServ <ej44@gr13b04n05.petrobras.biz>
2024-08-20 11:13:13 -0300

    Adicionando arquivos de modelo

---
+++ models/encoder.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+
+class Encoder(nn.Module):
+    def __init__(self, n_channels, z_size):
+        super(Encoder, self).__init__()
+        
+        self.n_channels = n_channels
+
+        self.main_module = nn.Sequential(
+            # Image (Cx256)
+            nn.Conv1d(in_channels=n_channels, out_channels=128, kernel_size=10, stride=2, padding=4),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            # State (128x128)
+            nn.Conv1d(in_channels=128, out_channels=256, kernel_size=10, stride=2, padding=4),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # State (256x64)
+            nn.Conv1d(in_channels=256, out_channels=512, kernel_size=10, stride=2, padding=4),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # State (512x32)
+            nn.Conv1d(in_channels=512, out_channels=1024, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+            # outptut of main module --> State (1024x16)
+        
+        self.input_last_layer = 1024*16
+        
+        self.out_feature = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(self.input_last_layer, z_size)
+        )
+    
+    def forward(self, x):
+        x = self.main_module(x)
+        x = self.out_feature(x)
+        x = x.unsqueeze(2)
+        return x
+
+    def use_checkpointing(self):
+        self.main_module = torch.utils.checkpoint(self.main_module)
+        self.out_feature = torch.utils.checkpoint(self.out_feature)
